2560b85661024d89f38e0e5111ea24f4e0375b05
Valeza Grainca <valzagrainca2002@gmail.com>
2023-01-27 09:23:41 +0100

    Change the sorting to desc

--- Angular/src/app/components/submarket-ranking/submarket-ranking.component.ts
+++ Angular/src/app/components/submarket-ranking/submarket-ranking.component.ts
@@ -16,7 +16,7 @@ export class SubmarketRankingComponent implements OnInit{
 
   ngOnInit(): void {
     this.submarketrankings=this.submarketservice.submarketrankings;
-    this.sortedRankingData = this.sortPipe.transform(this.submarketrankings, "asc", "price");
+    this.sortedRankingData = this.sortPipe.transform(this.submarketrankings, "desc", "price");
   }
 
   @Input() 
